5b8f69ce83e47d5f1a162c1be53c5ebc2df281cc
patison89 <patison89@gmail.com>
2018-04-05 15:57:24 +0100

    display property details on property page, add styling

---
+++ src/pages/property/property.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {PropertiesProvider} from "../../providers/properties/properties";
+
+/**
+ * Generated class for the PropertyPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-property',
+  templateUrl: 'property.html',
+})
+export class PropertyPage {
+
+  property;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private propertiesProvider: PropertiesProvider) {
+
+
+    this.property = this.navParams.data;
+
+  }
+
+  ngOnInit() {
+   this.propertiesProvider.getProperty(this.property.property_id).subscribe(response => console.log(response));
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PropertyPage', this.property);
+
+
+  }
+
+}
